543a4a715592bc5d2a2973066d4cdc184917165a
Chun Chie <chunchie93@gmail.com>
2025-10-08 17:27:10 +0800

    feat: add material dependencies

---
+++ src/styles/stylings.ts
@@ -0,0 +1,16 @@
+import { purple } from "@mui/material/colors";
+
+export const COLORS = {
+    red: "#c54e4b",
+    lightGreyBorder: '#D9D9D9',
+    textBlack: '#333333',
+    white: '#ffffff',
+    lightGrey: '#F9F9F9',
+    purple: purple[500],
+    backdropColor: '#00000033',
+};
+
+export const BORDERS = {
+    radiusCard: '0.75rem',
+    radiusForm: '0.4rem',
+}
