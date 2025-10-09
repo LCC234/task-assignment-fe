c78f658c157cd993d25d4511c1ce5a99015ee3a6
Chun Chie <chun-chie.lee@hpe.com>
2025-10-09 13:14:36 +0800

    feat: add red-light-opacity variable and update stylings with font constants

--- src/styles/stylings.ts
+++ src/styles/stylings.ts
@@ -8,9 +8,15 @@ export const COLORS = {
     lightGrey: '#F9F9F9',
     purple: purple[500],
     backdropColor: '#00000033',
+    redLight: "#fc6161",
+    lightGrey2: '#c0c0c0',
 };
 
 export const BORDERS = {
     radiusCard: '0.75rem',
     radiusForm: '0.4rem',
+}
+
+export const FONTS = {
+    default: 'Montserrat, sans-serif',
 }
